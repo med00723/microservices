74783bb9fba0d232faab216e3cf66725f708aa62
med00723 <mohamedalouane.ess@gmail.com>
2022-04-28 09:59:06 +0100

    laste

---
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -0,0 +1,131 @@
+package com.ecommerce.microcommerce.model;
+
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@JsonFilter("monFiltreDynamique")
+public class Product {
+
+
+
+    private int id;
+
+    private String nom;
+
+    private int prix;
+
+
+
+    //information que nous ne souhaitons pas exposer
+    @JsonIgnore
+    private int prixAchat;
+
+
+
+    //constructeur par défaut
+
+    public Product() {
+
+    }
+
+
+
+    //constructeur pour nos tests
+
+    public Product(int id, String nom, int prix, int prixAchat) {
+
+        this.id = id;
+
+        this.nom = nom;
+
+        this.prix = prix;
+
+        this.prixAchat = prixAchat;
+
+
+    }
+
+
+
+    public int getId() {
+
+        return id;
+
+    }
+
+
+
+    public void setId(int id) {
+
+        this.id = id;
+
+    }
+
+
+
+    public String getNom() {
+
+        return nom;
+
+    }
+
+
+
+    public void setNom(String nom) {
+
+        this.nom = nom;
+
+    }
+
+
+
+    public int getPrix() {
+
+        return prix;
+
+    }
+
+
+
+    public void setPrix(int prix) {
+
+        this.prix = prix;
+
+    }
+
+
+
+    public int getPrixAchat() {
+
+        return prixAchat;
+
+    }
+
+
+
+    public void setPrixAchat(int prixAchat) {
+
+        this.prixAchat = prixAchat;
+
+    }
+
+
+
+    @Override
+
+    public String toString() {
+
+        return "Product{" +
+
+                "id=" + id +
+
+                ", nom='" + nom + '\'' +
+
+                ", prix=" + prix +
+
+                '}';
+
+    }
+
+}
---
+++ src/main/java/com/ecommerce/microcommerce/web/dao/ProductDao.java
@@ -0,0 +1,19 @@
+package com.ecommerce.microcommerce.web.dao;
+import com.ecommerce.microcommerce.model.Product;
+import  org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public class ProductDao {
+    public List<Product> findAll() {
+        return null;
+    }
+
+    public Product findById(int id) {
+        return null;
+    }
+
+    public Product save(Product product) {
+        return null;
+    }
+}
